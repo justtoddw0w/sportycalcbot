fc8f82f196bdaffc8c0a7874a125d879d1adb33d
Todd <kl.work@mail.ru>
2019-07-10 15:35:45 +0300

    init rep

---
+++ src/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  TOKEN: '880401313:AAHKDjs5IHzVljJhqopxcslRzJQf8eptQhM',
+  DB_URL: 'mongodb://localhost/sportycalc'
+}
---
+++ src/keyboard-buttons.js
@@ -0,0 +1,13 @@
+module.exports = {
+  home: {
+    films: 'Цель',
+    favourite: 'Тренировки',
+    cinemas: 'Питание'
+  },
+  film: {
+    random: 'Похудание',
+    action: 'Удержание',
+    comedy: 'Набор'
+  },
+  back: 'Назад'
+}
